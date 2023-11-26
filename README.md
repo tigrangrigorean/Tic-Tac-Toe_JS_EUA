5ca7aa1100511c4aecb5b88f6d12fe1ac7a766bc
= <tigran_grigoryan05@inbox.ru>
2023-11-26 15:41:26 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+﻿Tic-Tac-Toe Game
+
+Welcome to the Tic-Tac-Toe game implemented in JavaScript! This version of the classic game introduces a strategic twist: player 'X' must place their move in the center to win.
+How to Play
+
+    Clone the repository to your local machine.
+
+    bash
+
+git clone https://github.com/your-username/tic-tac-toe.git
+
+Open the project directory.
+
+bash
+
+    cd tic-tac-toe
+
+    Open the index.html file in your web browser to start the game.
+
+    Follow the on-screen instructions to play the game.
+
+Rules
+
+    Player 'X' starts the game.
+    Player 'O' places their moves randomly.
+    To win, player 'X' must place their move in the center (5th position).
---
+++ script.js
@@ -0,0 +1,73 @@
+﻿let currentPlayer = 'X';
+let board = ['', '', '', '', '', '', '', '', ''];
+let gameActive = true;
+let winPosition = [];
+let xOrO = true;
+
+function handleClick(index) {
+  if (gameActive && board[index] === '') {
+    board[index] = currentPlayer;
+    document.getElementById('board').children[index].innerText = currentPlayer;
+
+    if (!checkWinner()) {
+      currentPlayer = 'O';
+      xOrO == true;
+      makeRandomMove();
+    }
+  }
+    if(checkWinner() && winPosition[0] == 3 && winPosition[1] == 4 && winPosition[2] == 5 && xOrO != false) {
+      document.getElementById("status").innerHTML = currentPlayer + " wins";
+      gameActive = false;
+    } else if(checkWinner() && xOrO == false) {
+      gameActive = false;
+    } else if (checkWinner()) {
+      gameActive = false;
+    }
+}
+
+function makeRandomMove() {
+  const emptyCells = board.reduce((acc, cell, index) => {
+    if (cell === '') {
+      acc.push(index);
+    }
+    return acc;
+  }, []);
+
+  if (emptyCells.length > 0) {
+    const randomIndex = emptyCells[Math.floor(Math.random() * emptyCells.length)];
+
+    board[randomIndex] = 'O';
+    document.getElementById('board').children[randomIndex].innerText = 'O';
+
+    currentPlayer = 'X';
+
+    if (checkWinner()) {
+      document.getElementById("status").innerHTML = "O wins";
+      xOrO = false;
+      gameActive = false;
+    } else if (board.every(cell => cell !== '')) {
+      document.getElementById("status").innerHTML = "Draw";
+      gameActive = false;
+    }
+  }
+}
+
+function checkWinner() {
+  const winPatterns = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], 
+    [0, 4, 8], [2, 4, 6]             
+  ];
+
+  for (const pattern of winPatterns) {
+    const [a, b, c] = pattern;
+    if (board[a] !== '' && board[a] === board[b] && board[a] === board[c]) {
+      winPosition[0] = a;
+      winPosition[1] = b;
+      winPosition[2] = c;
+      return true;
+    }
+  }
+
+  return false;
+}
